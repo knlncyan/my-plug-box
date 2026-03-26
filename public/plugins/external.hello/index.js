01aba6db61d751b29521231bcb84541a823da625
chenyan <chenyan@yanfuinvestments.com>
2026-03-26 12:23:56 +0800

    feat：最小外部案例

---
+++ public/plugins/external.hello/index.js
@@ -0,0 +1,25 @@
+﻿const plugin = {
+  pluginId: 'external.hello',
+  activate: async (api) => {
+    const storage = api.get('storage');
+    const launchCount = ((await storage.get('launch_count')) ?? 0) + 1;
+    await storage.set('launch_count', launchCount);
+    console.info(`[external.hello] activated, launch_count=${launchCount}`);
+  },
+  deactivate: () => {
+    console.info('[external.hello] deactivated');
+  },
+  commands: {
+    'external.hello.open': (context) => {
+      const views = context.api.get('views');
+      views.activate('external.hello.main');
+      return 'external.hello';
+    },
+    'external.hello.ping': () => {
+      console.info('[external.hello] pong');
+      return 'pong';
+    },
+  },
+};
+
+export default plugin;
---
+++ public/plugins/external.hello/view/index.js
@@ -0,0 +1,16 @@
+﻿const STYLE_ID = 'external-hello-view-style';
+
+function ensureStyles() {
+  if (document.getElementById(STYLE_ID)) return;
+  const link = document.createElement('link');
+  link.id = STYLE_ID;
+  link.rel = 'stylesheet';
+  link.href = '/plugins/external.hello/view/index.css';
+  document.head.appendChild(link);
+}
+
+export default function ExternalHelloView(props) {
+  ensureStyles();
+  const hint = typeof props?.welcome === 'string' ? props.welcome : 'external plugin';
+  return `External Hello View (${hint})`;
+}
